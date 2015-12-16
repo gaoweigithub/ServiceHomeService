7aaa1681c954af812bc90629fdcfc127777b4c52
gaowei <gaowei.hnu@hotmail.com>
2015-12-16 22:35:44 +0800

    取消nuget包依赖

--- ServiceHome/ServiceHome/DAL/GetServices.cs
+++ ServiceHome/ServiceHome/DAL/GetServices.cs
@@ -18,26 +18,61 @@ namespace ServiceHome.DAL
         /// </summary>
         /// <param name="cityID"></param>
         /// <returns></returns>
-        public List<ServiceHomeDB.SERVICE> GetServiceList(int cityID)
+        public List<Services.showService> GetServiceList(int cityID)
         {
             //cityid不做判断是否开通  前台默认绑定开通
             //从缓存获取，获取不到则请求数据库，然后更新缓存
-            List<ServiceHomeDB.SERVICE> listService = new List<ServiceHomeDB.SERVICE>();
+            //List<ServiceHomeDB.SERVICE> listService = new List<ServiceHomeDB.SERVICE>();
+            List<Services.showService> listService = null;
+
             string key = ConstString.GetCityServiceKey(cityID);
-            if (MemoryCacheHelper.IsExist(key))
+            if (MemoryCacheHelper.IsExist(key) && MemoryCacheHelper.GetValue(key) != null)
             {
                 //缓存
-                listService = MemoryCacheHelper.GetValue(key) as List<ServiceHomeDB.SERVICE>;
+                listService = MemoryCacheHelper.GetValue(key) as List<Services.showService>;
             }
             else
             {
+                listService = new List<Services.showService>();
                 //数据库
                 using (ServiceHomeDB.housekeepingEntities db = new ServiceHomeDB.housekeepingEntities())
                 {
                     string sql = @"SELECT S.* FROM CITY C INNER JOIN CITY_SERVICE CS ON C.CITYID=CS.CITYID 
                                     INNER JOIN SERVICE S ON CS.SERVICE_ID=S.SERVICE_ID
                                     WHERE C.CITYID=@CITYID AND C.ISOPEN=1";
-                    listService = db.SERVICE.SqlQuery(sql, new SqlParameter("@CITYID", cityID)).ToList();
+                    List<ServiceHomeDB.SERVICE> sl = db.SERVICE.SqlQuery(sql, new SqlParameter("@CITYID", cityID)).ToList();
+
+
+                    foreach (var item in sl)
+                    {
+                        if (item.ISLEAF.HasValue && !item.ISLEAF.Value)
+                        {
+                            Services.showService parentService = new Services.showService
+                            {
+                                SERVICE_ID = item.SERVICE_ID.ToString(),
+                                SERVICE_CODE = item.SERVICE_CODE,
+                                SERVICE_NAME = item.SERVICE_NAME,
+                                PICURL = item.PICURL,
+                                SERVICE_DESC = item.SERVICE_ITEMS,
+                                SERVICE_ITEMS = new List<Services.showService>()
+                            };
+                            foreach (var ch in sl)
+                            {
+                                if (ch.PARENT_SERVICE_ID.HasValue && ch.PARENT_SERVICE_ID.Value == item.SERVICE_ID)
+                                {
+                                    parentService.SERVICE_ITEMS.Add(new Services.showService
+                                    {
+                                        SERVICE_ID = ch.SERVICE_ID.ToString(),
+                                        SERVICE_CODE = ch.SERVICE_CODE,
+                                        SERVICE_NAME = ch.SERVICE_NAME,
+                                        PICURL = ch.PICURL,
+                                        SERVICE_DESC = ch.SERVICE_ITEMS
+                                    });
+                                }
+                            }
+                            listService.Add(parentService);
+                        }
+                    }
 
                     //更新缓存
                     //半小时过期
--- ServiceHome/ServiceHome/Services/GetCityService.cs
+++ ServiceHome/ServiceHome/Services/GetCityService.cs
@@ -17,6 +17,7 @@ namespace ServiceHome.Services
         {
             GetCityServiceResponse response = new GetCityServiceResponse();
             response.ServiceList = new DAL.GetServices().GetServiceList(request.cityID);
+
             response.ResponseStatus = new Model.Common.ResponseStatus { isSuccess = true };
             return response;
         }
@@ -33,6 +34,18 @@ namespace ServiceHome.Services
     public class GetCityServiceResponse : ResponseBase
     {
         //服务列标
-        public List<ServiceHomeDB.SERVICE> ServiceList { get; set; }
+        //public List<ServiceHomeDB.SERVICE> ServiceList { get; set; }
+        public List<showService> ServiceList { get; set; }
+
+    }
+    public class showService
+    {
+        public string SERVICE_ID { get; set; }
+        public string PARENT_SERVICE_ID { get; set; }
+        public string SERVICE_CODE { get; set; }
+        public string SERVICE_NAME { get; set; }
+        public string PICURL { get; set; }
+        public string SERVICE_DESC { get; set; }
+        public List<showService> SERVICE_ITEMS { get; set; }
     }
 }
